b640110e4b355eaa944e645d9fb2b8dd3ba94064
surya-10 <sp659151@gmail.com>
2023-08-08 02:52:58 +0530

    final

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 let fs = require("fs");
 let express = require("express");
 let path = require("path");
+const mime = require('mime');
 
 let dirPath = path.join(__dirname, "timestamp");
 
 let app = express();
 
-let port = 9000
+let port = 9001
 
 app.listen(port, ()=>{
     console.log("connected");
 })
-console.log(__dirname)
+console.log(dirPath)
 
 app.get("/time", (req, res)=>{
     let time = new Date();
@@ -22,4 +23,22 @@ app.get("/time", (req, res)=>{
         }
     })
     res.sendFile(path.join(dirPath, "current time-stamp.txt"))
+})
+
+
+
+
+app.get("/text-files", (req, res)=>{
+    let dir = path.join(__dirname, "dir");
+    
+    fs.readdir(dir, (err, files)=>{
+        if(err){
+            res.send("error reading directory");
+            
+        }
+        const textFiles = files.filter(file => path.extname(file).toLowerCase() === '.txt');
+        res.json({textFiles});
+
+    })
+    
 })
